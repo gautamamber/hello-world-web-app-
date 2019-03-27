e71c906458bdf58ddf18e07bada64d51a0274afc
gautamamber <ambergautam1@gmail.com>
2019-03-27 14:21:45 +0530

    csv import into model from admin

--- hello_world_app/uploadcsvadmin/admin.py
+++ hello_world_app/uploadcsvadmin/admin.py
@@ -1,4 +1,45 @@
 from django.contrib import admin
 from .models import Student
-admin.site.register(Student)
-# Register your models here.
+from django import forms
+import csv
+from django.shortcuts import render, redirect
+from django.urls import path
+
+class StudentModelForm(forms.Form):
+	csv_file = forms.FileField(required = False, label = "Please select a file")
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+	change_list_template = "admin/uploadcsvadmin/student/change_list.html"
+
+	def get_urls(self):
+		urls = super().get_urls()
+		my_urls = [
+			path('import-csv/', self.import_csv),
+		]
+		return my_urls + urls
+
+	def import_csv(self, request):
+		if request.method == "POST":
+			csv_file = request.FILES["csv_file"]
+			file_data = csv_file.read().decode("utf-8")
+			lines = file_data.split('\n')
+			lines.pop(0)
+			for line in lines:
+				field = line.split(',')
+				if field[0]:
+					data = Student.objects.create(
+							name = field[0],
+							roll_no = field[1],
+							stu_class = field[2],
+							school = field[3],
+							city = field[4],
+						)
+			self.message_user(request, "Your csv file has been imported")
+
+		form = StudentModelForm()
+		payload = {"form":form}
+		return render(request, "admin/uploadcsvadmin/student/csv_form.html", payload)
+
+
+
--- hello_world_app/uploadcsvadmin/models.py
+++ hello_world_app/uploadcsvadmin/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Student(models.Model):
 	name = models.CharField(max_length = 20)
-	roll_no = models.IntegerField()
-	stu_class = models.CharField(max_length = 20)
-	school = models.CharField(max_length = 100)
-	city = models.CharField(max_length = 20)
+	roll_no = models.IntegerField(blank = True, null = True)
+	stu_class = models.CharField(max_length = 20, blank = True, null = True)
+	school = models.CharField(max_length = 100, blank = True, null = True)
+	city = models.CharField(max_length = 20, blank = True, null = True)
 
 	def __str__(self):
 		return str(self.name)
